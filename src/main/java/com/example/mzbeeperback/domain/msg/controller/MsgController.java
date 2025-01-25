44ae7173c164b901d29093a5ef64525bda28d941
narumi <sychoi2008@sookmyung.ac.kr>
2025-01-25 19:16:49 +0900

    1. 중복 코드 메세지로 뺌

--- src/main/java/com/example/mzbeeperback/domain/msg/controller/MsgController.java
+++ src/main/java/com/example/mzbeeperback/domain/msg/controller/MsgController.java
@@ -19,25 +19,26 @@ public class MsgController {
     @PostMapping("/msg")
     public ResponseEntity<String> sendMsg(@RequestHeader("Authorization") String accessToken, @RequestBody MsgDTO msgDTO) {
         // JWT 토큰 자르기
-        String parsedToken = accessToken.replace("Bearer ", "");
-        msgService.saveMsg(parsedToken, msgDTO);
+        msgService.saveMsg(getParsedToken(accessToken), msgDTO);
         return new ResponseEntity<>("메세지 보내기 성공", HttpStatus.CREATED);
     }
 
 
     // 내가 보낸 모든 메세지 조회하기
+
     @GetMapping("/myinfo/mysentlist")
     public List<MySentMsgDTO> mySendMsg(@RequestHeader("Authorization") String accessToken) {
-        String parsedAccessToken = accessToken.replace("Bearer ", "");
-        return msgService.getMySend(parsedAccessToken);
+        return msgService.getMySend(getParsedToken(accessToken));
     }
-
     // 버튼 눌러서 최신 삐삐 메세지 1개를 프론트에게 반환하기
+
     @GetMapping("/mymsg")
     public String getBeeperMsg(@RequestHeader("Authorization") String accessToken) {
-        String parsedAccessToken = accessToken.replace("Bearer ", "");
-        return msgService.getMsg(parsedAccessToken);
+        return msgService.getMsg(getParsedToken(accessToken));
     }
 
+    private static String getParsedToken(String accessToken) {
+        return accessToken.replace("Bearer ", "");
+    }
 
 }
